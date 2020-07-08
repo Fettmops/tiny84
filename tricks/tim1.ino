1ded6d6e72c202d5bd5f18421e8f3e09a5436760
Fettmops <Fettmops@users.noreply.github.com>
2020-07-08 18:39:39 +0200

    Create tim1.ino

---
+++ tricks/tim1.ino
@@ -0,0 +1,31 @@
+#define ledPin PA1
+
+void setupTimer1() {
+  noInterrupts();
+  // Clear registers
+  TCCR1A = 0;
+  TCCR1B = 0;
+  TCNT1 = 0;
+
+  // 1 Hz (1000000/((15624+1)*64))
+  OCR1A = 15624;
+  // CTC
+  TCCR1B |= (1 << WGM12);
+  // Prescaler 64
+  TCCR1B |= (1 << CS11) | (1 << CS10);
+  // Output Compare Match A Interrupt Enable
+  TIMSK1 |= (1 << OCIE1A);
+  interrupts();
+}
+
+void setup() {
+  pinMode(ledPin, OUTPUT);
+  setupTimer1();
+}
+
+void loop() {
+}
+
+ISR(TIM1_COMPA_vect) {
+  digitalWrite(ledPin, digitalRead(ledPin) ^ 1);
+}
